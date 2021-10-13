d84aa35913da57f2f0dadab4216fa7ec4bb9b807
german nunez <german90nunez@gmail.com>
2021-10-13 18:42:53 -0500

    setting up project to local testing, running nodemon on file

---
+++ minesweeper.js
@@ -0,0 +1,11 @@
+//
+// This is only a SKELETON file for the 'Minesweeper' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+// re add export to function, running nodemon
+const annotate = (input) => {
+  console.log(input)
+};
+
+annotate([' * * ', '  *  ', '  *  ', '     '])
